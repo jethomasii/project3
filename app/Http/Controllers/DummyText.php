bb56a0b65884e36363f138e8aa9819f0efb8bde7
James Thomas <jethomasii@gmail.com>
2016-10-26 19:47:51 -0500

    actually added a package that works

--- app/Http/Controllers/DummyText.php
+++ app/Http/Controllers/DummyText.php
@@ -6,12 +6,27 @@ use Illuminate\Http\Request;
 
 use project3\Http\Requests;
 
+use Magyarjeti\Loripsum\Client;
+use Magyarjeti\Loripsum\Http\CurlAdapter;
+
 class DummyText extends Controller
 {
     //
     public function index()
     {
-      return 'Dummy Text';
-      return LoremServiceProvider(1);
+
+      // Instantiate the "client" to generate text
+      $client = new Client(new CurlAdapter);
+
+      // Set normal variables.
+      $x = 5;
+      $text ='';
+
+      for ($y = 0; $y < $x; $y++) {
+        $text = $text . '<p>' .  $client->medium()->text(3); . '</p>';
+      }
+
+      return $text;
     }
+
 }
