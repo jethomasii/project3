c7569dcd5883867f5d8ab43d67c6bf7ebf74ccfa
James Thomas <james_thomas@apple.com>
2016-10-27 19:02:31 -0500

    changed for spacing on landing

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -6,10 +6,8 @@
 
 
               <a href='/dummytext/create'><h2>Generate DummyText</h2></a>
-              <br>
               <p>Ever thought, "Hey I should make some lorem ipsum text for no reason?" Well now you can!</p>
               <a href='/createuserdata'><h2>Make Some Users</h2></a>
-              <br>
               <p>These user names are ridiculous, ridiculous I say!</p>
 
 
